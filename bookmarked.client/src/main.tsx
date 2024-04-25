c45b5adfcf068964246d667361739e0c1acac0b6
DJJD13 <jdeggert98@gmail.com>
2024-04-24 22:07:48 -0500

    Removed ! from Main.tsx, use as HTMLElement instead

--- bookmarked.client/src/main.tsx
+++ bookmarked.client/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
         <RouterProvider router={router} />
   </React.StrictMode>,
