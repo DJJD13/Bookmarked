5b1dfd3a55f985b88cf79b8ad9dfa8aaa663b811
DJJD13 <jdeggert98@gmail.com>
2024-04-27 17:25:45 -0500

    Moved certificate generator to function

--- bookmarked.client/vite.config.ts
+++ bookmarked.client/vite.config.ts
@@ -4,36 +4,36 @@ import { defineConfig } from 'vitest/config';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
+import /* child_process */ { execSync } from 'child_process';
+// import { env } from 'process';
 import tailwindcss from "tailwindcss";
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "bookmarked.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7170';
-
+// const baseFolder =
+//     env.APPDATA !== undefined && env.APPDATA !== ''
+//         ? `${env.APPDATA}/ASP.NET/https`
+//         : `${env.HOME}/.aspnet/https`;
+//
+// const certificateName = "bookmarked.client";
+// const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+// const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+//
+// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//     if (0 !== child_process.spawnSync('dotnet', [
+//         'dev-certs',
+//         'https',
+//         '--export-path',
+//         certFilePath,
+//         '--format',
+//         'Pem',
+//         '--no-password',
+//     ], { stdio: 'inherit', }).status) {
+//         throw new Error("Could not create certificate.");
+//     }
+// }
+//
+// const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7170';
+//
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
@@ -49,17 +49,8 @@ export default defineConfig({
         }
     },
     server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
-            }
-        },
         port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+        https: generateCerts() 
     },
     test: {
         environment: 'jsdom',
@@ -67,3 +58,47 @@ export default defineConfig({
         setupFiles: './tests/setup.ts'
     }
 })
+
+function generateCerts() {
+    const baseFolder =
+        process.env.APPDATA !== undefined && process.env.APPDATA !== ""
+            ? `${process.env.APPDATA}/ASP.NET/https`
+            : `${process.env.HOME}/.aspnet/https`;
+    const certificateArg = process.argv
+        .map((arg) => arg.match(/--name=(?<value>.+)/i))
+        .filter(Boolean)[0];
+    const certificateName = certificateArg
+        ? certificateArg.groups!.value
+        : process.env.npm_package_name;
+
+    if (!certificateName) {
+        console.error(
+            "Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
+        );
+        process.exit(-1);
+    }
+
+    const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+    const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        const outp = execSync(
+            "dotnet " +
+            [
+                "dev-certs",
+                "https",
+                "--export-path",
+                certFilePath,
+                "--format",
+                "Pem",
+                "--no-password",
+            ].join(" ")
+        );
+        console.log(outp.toString());
+    }
+
+    return {
+        cert: fs.readFileSync(certFilePath, "utf8"),
+        key: fs.readFileSync(keyFilePath, "utf8"),
+    };
+}
